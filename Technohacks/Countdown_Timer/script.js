46a3e00de23e0246dac7a815d501e79dca427da0
mahendranasokan <127123989+mahendranasokan@users.noreply.github.com>
2023-12-31 14:59:12 +0530

    Update script.js

--- Technohacks/Countdown_Timer/script.js
+++ Technohacks/Countdown_Timer/script.js
@@ -1,16 +1,12 @@
 const btnStart=document.querySelector('.start');
 const btnStop=document.querySelector('.stop');
 const btnReset=document.querySelector('.reset');
-
 let hrs=min=sec=ms=0,startTimer;
-
 btnStart.addEventListener('click',()=>{
-
   btnStart.classList.add('start-active');
   btnStop.classList.remove('stop-active');
-
   startTimer=setInterval(()=>{
-    ms++;//ms=ms+1;
+    ms++;
     if(ms==100){
       sec++;
       ms=0;
@@ -26,14 +22,11 @@ btnStart.addEventListener('click',()=>{
     updateDisplay();
   },10);
 });
-
 btnStop.addEventListener('click',()=>{
   clearInterval(startTimer);
   btnStart.classList.remove('start-active');
   btnStop.classList.add('stop-active');
-
 });
-
 btnReset.addEventListener('click',()=>{
   hrs=min=sec=ms=0;
   clearInterval(startTimer);
@@ -41,15 +34,11 @@ btnReset.addEventListener('click',()=>{
   btnStart.classList.remove('start-active');
   btnStop.classList.remove('stop-active');
 });
-
-
 function updateDisplay(){
-  //Formated Display
   phrs=hrs<10?'0'+hrs:hrs;
   pmin=min<10?'0'+min:min;
   psec=sec<10?'0'+sec:sec;
   pms=ms<10?'0'+ms:ms;
-  //Output
   document.querySelector('.hrs').innerText=phrs;
   document.querySelector('.min').innerText=pmin;
   document.querySelector('.sec').innerText=psec;
